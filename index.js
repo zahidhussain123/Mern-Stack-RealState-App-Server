216aacd00e964955ec1b86eda439066f12f96b7f
Zahid Hussain <zahid.hussain@biz-analytix.com>
2024-06-29 15:17:26 +0500

    cors changees

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 4000
 app.use(express.json())
-app.use(cors({ origin: "https://mern-stack-real-state-app-client-oc40vkkl0.vercel.app" }));
+app.use(cors());
 app.use(cookieParser());
 app.use((req, res, next) => {
     console.log(`Incoming request: ${req.method} ${req.url}`);
