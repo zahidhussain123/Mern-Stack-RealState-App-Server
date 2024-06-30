3e6c0386711c373812588bfcee0b6a64b455413d
Zahid Hussain <zahid.hussain@biz-analytix.com>
2024-06-30 12:47:34 +0500

    server issue resolve

--- controllers/residencyController.js
+++ controllers/residencyController.js
@@ -3,7 +3,6 @@ import { prisma } from "../config/prismaConfig.js";
 
 
 export const createResidency = asyncHandler(async (req, res) => {
-    console.log("res create ", req.body.data)
     let { title, description, price, address, city, country, image, facilities, userEmail } = req.body.data
     try {
         const residency = await prisma.residency.create({
